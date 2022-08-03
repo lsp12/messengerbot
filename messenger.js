95f9c8b10232f4b6e397eecec785bafd2c22e4d5
CrisLDev <leonardomoreirapazmio@gmail.com>
2022-08-03 15:17:26 -0500

    added bot initialization

---
+++ messenger.js
@@ -0,0 +1,32 @@
+import express from "express"
+import bodyParser from "body-parser"
+import request from "request"
+
+var app = express()
+
+app.use (bodyParser.urlencoded ({extended: false}));
+
+app.use(bodyParser.json())
+
+app.listen(3210, function(){
+    console.log("Server Listen in localhost:3210")
+})
+
+app.get("/", function(req, res){
+    res.send("Desde el pc")
+})
+
+app.get("/webhook", function(req, res){
+    console.log(req.query, "hola")
+    if(req.query['hub.verify_token'] === "hello_token"){
+        console.log(req.query)
+        res.send(req.query["hub.challenge"])
+    }else{
+    res.send("Prueba")
+    }
+})
+
+app.post("/webhook", function(req, res){
+    res.sendStatus(200)
+})
+
