a36ae8a3405591631f9e235905c40ced0f02f490
jonathan vera <jonathankenny852@gmail.com>
2022-09-25 13:29:53 -0500

    atach

--- src/messenger.js
+++ src/messenger.js
@@ -54,6 +54,63 @@ app.post("/webhook", function (req, res) {
               attachment: {
                 type: "template",
                 payload: {
+                  template_type: "receipt",
+                  recipient_name: "Stephane Crozatier",
+                  order_number: "12345678902",
+                  currency: "USD",
+                  payment_method: "Visa 2345",
+                  order_url:
+                    "http://petersapparel.parseapp.com/order?order_id=123456",
+                  timestamp: "1428444852",
+                  address: {
+                    street_1: "1 Hacker Way",
+                    street_2: "",
+                    city: "Menlo Park",
+                    postal_code: "94025",
+                    state: "CA",
+                    country: "US",
+                  },
+                  summary: {
+                    subtotal: 75.0,
+                    shipping_cost: 4.95,
+                    total_tax: 6.19,
+                    total_cost: 56.14,
+                  },
+                  adjustments: [
+                    {
+                      name: "New Customer Discount",
+                      amount: 20,
+                    },
+                    {
+                      name: "$10 Off Coupon",
+                      amount: 10,
+                    },
+                  ],
+                  elements: [
+                    {
+                      title: "Classic White T-Shirt",
+                      subtitle: "100% Soft and Luxurious Cotton",
+                      quantity: 2,
+                      price: 50,
+                      currency: "USD",
+                      image_url:
+                        "http://petersapparel.parseapp.com/img/whiteshirt.png",
+                    },
+                    {
+                      title: "Classic Gray T-Shirt",
+                      subtitle: "100% Soft and Luxurious Cotton",
+                      quantity: 1,
+                      price: 25,
+                      currency: "USD",
+                      image_url:
+                        "http://petersapparel.parseapp.com/img/grayshirt.png",
+                    },
+                  ],
+                },
+              },
+              /* attachment: {
+                type: "template",
+                payload: {
                   template_type: "generic",
                   elements: [
                     {
@@ -104,7 +161,7 @@ app.post("/webhook", function (req, res) {
                     },
                   ],
                 },
-              },
+              }, */
 
               /* text: `Solo se repetir el mensaje: a`, */
               /* attachment: {
@@ -136,7 +193,10 @@ app.post("/webhook", function (req, res) {
           request(
             {
               uri: "https://graph.facebook.com/v2.6/me/messages",
-              qs: { access_token: process.env.APP_TOKEN },
+              qs: {
+                access_token:
+                  EAAGTM3jnwakBANlp24emKdxPshuaLNgVIKVdkDJmLsBy5RKLFaaFD5oqz28xIZAbPzgYyMYmOLToiTKOqVZAvHbegQkC4kZA5betZAsZAUypSx11mryb7xQo7sZCgJpPhPSPP0jJnGhSeowTfbq7xKScUvkfGJSwh0QwNc3518IPQzxgUdZCug5,
+              },
               method: "POST",
               json: messageResponseData,
             },
