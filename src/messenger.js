590e76e71fc29aa8842627e3e7428fe2f59352e6
jonathan vera <jonathankenny852@gmail.com>
2022-09-25 12:13:26 -0500

    data

--- src/messenger.js
+++ src/messenger.js
@@ -71,32 +71,7 @@ app.post("/webhook", function (req, res) {
                       buttons: [
                         {
                           type: "web_url",
-                          url: "https://petersfancybrownhats.com",
-                          title: "View Website",
-                        },
-                        {
-                          type: "postback",
-                          title: "Start Chatting",
-                          payload: "DEVELOPER_DEFINED_PAYLOAD",
-                        },
-                      ],
-                    },
-                    {
-                      title: "Welcome!",
-                      image_url:
-                        "https://assets.puzzlefactory.pl/puzzle/316/243/original.jpg",
-                      subtitle: "We have the right hat for everyone.",
-                      default_action: {
-                        type: "web_url",
-                        url: "https://petersfancybrownhats.com/view?item=103",
-                        messenger_extensions: false,
-                        webview_height_ratio: "tall",
-                        fallback_url: "https://petersfancybrownhats.com/",
-                      },
-                      buttons: [
-                        {
-                          type: "web_url",
-                          url: "https://petersfancybrownhats.com",
+                          url: "https://petersfancybrownhats.com/",
                           title: "View Website",
                         },
                         {
