5236776c4ad4311f1347f095c406701871dc5de0
jonathan vera <jonathankenny852@gmail.com>
2022-09-25 10:51:10 -0500

    add reply

--- src/messenger.js
+++ src/messenger.js
@@ -49,6 +49,20 @@ app.post("/webhook", function (req, res) {
             },
             message: {
               text: `Solo se repetir el mensaje: ${messageText}`,
+              quick_replies: [
+                {
+                  content_type: "text",
+                  title: "Red",
+                  payload: "<POSTBACK_PAYLOAD>",
+                  image_url: "http://example.com/img/red.png",
+                },
+                {
+                  content_type: "text",
+                  title: "Green",
+                  payload: "<POSTBACK_PAYLOAD>",
+                  image_url: "http://example.com/img/green.png",
+                },
+              ],
             },
           };
 
