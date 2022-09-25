48aca25081fdc9e7ed2dae3e7467a16581e243f0
jonathan vera <jonathankenny852@gmail.com>
2022-09-25 14:55:58 -0500

    comi

--- src/messenger.js
+++ src/messenger.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import request from "request";
-/* import "./database.js"; */
 
 var app = express();
 
@@ -14,7 +13,7 @@ app.listen(process.env.PORT || 3210, function () {
 });
 
 app.get("/", function (req, res) {
-  res.send("ChatbotMessage");
+  res.send("Desde el pc");
 });
 
 app.get("/webhook", function (req, res) {
@@ -30,8 +29,7 @@ app.post("/webhook", function (req, res) {
   if (data.object === "page") {
     data.entry.forEach((pageEntry) => {
       pageEntry.messaging.forEach((messagingEvent) => {
-        if (messagingEvent.message.payload) {
-          console.log(messagingEvent);
+        if (messagingEvent.message) {
           const senderId = messagingEvent.sender.id;
           let messageText = messagingEvent.message.text;
           if (
@@ -43,352 +41,29 @@ app.post("/webhook", function (req, res) {
             console.log(
               messagingEvent.message.attachments[0].payload.coordinates
             );
-            //cordenadas
-            /* messageText = `Latitud: ${messagingEvent.message.attachments[0].payload.coordinates.lat}, Longitud: ${messagingEvent.message.attachments[0].payload.coordinates.long}`; */
+            messageText = `Latitud: ${messagingEvent.message.attachments[0].payload.coordinates.lat}, Longitud: ${messagingEvent.message.attachments[0].payload.coordinates.long}`;
           }
-          const messageResponse = {
-            recipient: {
-              id: senderId,
-            },
-            message: {},
-          };
-          console.log(messageText);
-          switch (messageText.toLowerCase()) {
-            case "texto":
-              messageResponse.message = {
-                text: `Hola este es un texto`,
-              };
-              console.log("texto");
-              break;
-            case "botones":
-              console.log("botones");
-              messageResponse.message = {
-                attachment: {
-                  type: "template",
-                  payload: {
-                    template_type: "button",
-                    text: "What do you want to do next?",
-                    buttons: [
-                      {
-                        type: "web_url",
-                        url: "https://www.messenger.com",
-                        title: "Visit Messenger",
-                      },
-                    ],
-                  },
-                },
-              };
-              break;
-
-            case "imagenes":
-              console.log("imagenes");
-              messageResponse.message = {
-                attachment: {
-                  type: "image",
-                  payload: {
-                    url: "https://assets.puzzlefactory.pl/puzzle/316/243/original.jpg",
-                    is_reusable: true,
-                  },
-                },
-              };
-              break;
-
-            case "Checkout":
-              console.log("Checkout");
-              messageResponse.message = {
-                attachment: {
-                  type: "template",
-                  payload: {
-                    template_type: "receipt",
-                    recipient_name: "Stephane Crozatier",
-                    order_number: "12345678902",
-                    currency: "USD",
-                    payment_method: "Visa 2345",
-                    order_url:
-                      "http://petersapparel.parseapp.com/order?order_id=123456",
-                    timestamp: "1428444852",
-                    address: {
-                      street_1: "1 Hacker Way",
-                      street_2: "",
-                      city: "Menlo Park",
-                      postal_code: "94025",
-                      state: "CA",
-                      country: "US",
-                    },
-                    summary: {
-                      subtotal: 75.0,
-                      shipping_cost: 4.95,
-                      total_tax: 6.19,
-                      total_cost: 56.14,
-                    },
-                    adjustments: [
-                      {
-                        name: "New Customer Discount",
-                        amount: 20,
-                      },
-                      {
-                        name: "$10 Off Coupon",
-                        amount: 10,
-                      },
-                    ],
-                    elements: [
-                      {
-                        title: "Classic White T-Shirt",
-                        subtitle: "100% Soft and Luxurious Cotton",
-                        quantity: 2,
-                        price: 50,
-                        currency: "USD",
-                        image_url:
-                          "https://scontent.fgye1-2.fna.fbcdn.net/v/t39.30808-6/308928362_620241309750206_6506868414479100338_n.jpg?stp=dst-jpg_p180x540&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UVa9_r-2CJoAX-9YlQb&_nc_ht=scontent.fgye1-2.fna&oh=00_AT9mnY0TUq_NqVcsHcx2Vs38I0R2wzo0olRqMdlR0VPWFg&oe=6335BFA7",
-                      },
-                      {
-                        title: "Classic Gray T-Shirt",
-                        subtitle: "100% Soft and Luxurious Cotton",
-                        quantity: 1,
-                        price: 25,
-                        currency: "USD",
-                        image_url:
-                          "https://scontent.fgye1-2.fna.fbcdn.net/v/t39.30808-6/308928362_620241309750206_6506868414479100338_n.jpg?stp=dst-jpg_p180x540&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UVa9_r-2CJoAX-9YlQb&_nc_ht=scontent.fgye1-2.fna&oh=00_AT9mnY0TUq_NqVcsHcx2Vs38I0R2wzo0olRqMdlR0VPWFg&oe=6335BFA7",
-                      },
-                    ],
-                  },
-                },
-              };
-              break;
-
-            case "empezar":
-              console.log("empezar");
-              messageResponse.message = {
-                text: `Hola soy un bot`,
-                quick_replies: [
-                  {
-                    content_type: "text",
-                    title: "imagenes",
-                    payload: "<POSTBACK_PAYLOAD>",
-                    image_url:
-                      "https://assets.puzzlefactory.pl/puzzle/316/243/original.jpg",
-                  },
-                  {
-                    content_type: "text",
-                    title: "carrusel",
-                    payload: "<POSTBACK_PAYLOAD>",
-                    image_url:
-                      "https://i.pinimg.com/564x/f6/e1/90/f6e190ba3a2c8646e634908ba2e1443d.jpg",
-                  },
-                  {
-                    content_type: "text",
-                    title: "texto",
-                    payload: "<POSTBACK_PAYLOAD>",
-                    image_url:
-                      "https://i.pinimg.com/564x/f6/e1/90/f6e190ba3a2c8646e634908ba2e1443d.jpg",
-                  },
-                  {
-                    content_type: "text",
-                    title: "botones",
-                    payload: "<POSTBACK_PAYLOAD>",
-                    image_url:
-                      "https://i.pinimg.com/564x/f6/e1/90/f6e190ba3a2c8646e634908ba2e1443d.jpg",
-                  },
-                ],
-              };
-              break;
-
-            default:
-              messageResponse.message = {
-                text: "esto es lo que puedo hacer",
-                quick_replies: [
-                  {
-                    content_type: "text",
-                    title: "imagenes",
-                    payload: "<POSTBACK_PAYLOAD>",
-                    image_url:
-                      "https://assets.puzzlefactory.pl/puzzle/316/243/original.jpg",
-                  },
-                  {
-                    content_type: "text",
-                    title: "carrusel",
-                    payload: "<POSTBACK_PAYLOAD>",
-                    image_url:
-                      "https://i.pinimg.com/564x/f6/e1/90/f6e190ba3a2c8646e634908ba2e1443d.jpg",
-                  },
-                  {
-                    content_type: "text",
-                    title: "texto",
-                    payload: "<POSTBACK_PAYLOAD>",
-                    image_url:
-                      "https://i.pinimg.com/564x/f6/e1/90/f6e190ba3a2c8646e634908ba2e1443d.jpg",
-                  },
-                  {
-                    content_type: "text",
-                    title: "botones",
-                    payload: "<POSTBACK_PAYLOAD>",
-                    image_url:
-                      "https://i.pinimg.com/564x/f6/e1/90/f6e190ba3a2c8646e634908ba2e1443d.jpg",
-                  },
-                ],
-              };
-              break;
-          }
-
           const messageResponseData = {
             recipient: {
               id: senderId,
             },
             message: {
-              /* attachment: {
-                type: "template",
-                payload: {
-                  template_type: "receipt",
-                  recipient_name: "Stephane Crozatier",
-                  order_number: "12345678902",
-                  currency: "USD",
-                  payment_method: "Visa 2345",
-                  order_url:
-                    "http://petersapparel.parseapp.com/order?order_id=123456",
-                  timestamp: "1428444852",
-                  address: {
-                    street_1: "1 Hacker Way",
-                    street_2: "",
-                    city: "Menlo Park",
-                    postal_code: "94025",
-                    state: "CA",
-                    country: "US",
-                  },
-                  summary: {
-                    subtotal: 75.0,
-                    shipping_cost: 4.95,
-                    total_tax: 6.19,
-                    total_cost: 56.14,
-                  },
-                  adjustments: [
-                    {
-                      name: "New Customer Discount",
-                      amount: 20,
-                    },
-                    {
-                      name: "$10 Off Coupon",
-                      amount: 10,
-                    },
-                  ],
-                  elements: [
-                    {
-                      title: "Classic White T-Shirt",
-                      subtitle: "100% Soft and Luxurious Cotton",
-                      quantity: 2,
-                      price: 50,
-                      currency: "USD",
-                      image_url:
-                        "https://scontent.fgye1-2.fna.fbcdn.net/v/t39.30808-6/308928362_620241309750206_6506868414479100338_n.jpg?stp=dst-jpg_p180x540&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UVa9_r-2CJoAX-9YlQb&_nc_ht=scontent.fgye1-2.fna&oh=00_AT9mnY0TUq_NqVcsHcx2Vs38I0R2wzo0olRqMdlR0VPWFg&oe=6335BFA7",
-                    },
-                    {
-                      title: "Classic Gray T-Shirt",
-                      subtitle: "100% Soft and Luxurious Cotton",
-                      quantity: 1,
-                      price: 25,
-                      currency: "USD",
-                      image_url:
-                        "https://scontent.fgye1-2.fna.fbcdn.net/v/t39.30808-6/308928362_620241309750206_6506868414479100338_n.jpg?stp=dst-jpg_p180x540&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UVa9_r-2CJoAX-9YlQb&_nc_ht=scontent.fgye1-2.fna&oh=00_AT9mnY0TUq_NqVcsHcx2Vs38I0R2wzo0olRqMdlR0VPWFg&oe=6335BFA7",
-                    },
-                  ],
-                },
-              }, */
-              /* attachment: {
-                type: "template",
-                payload: {
-                  template_type: "generic",
-                  elements: [
-                    {
-                      title: "Welcome!",
-                      image_url:
-                        "https://scontent.fgye1-2.fna.fbcdn.net/v/t39.30808-6/308928362_620241309750206_6506868414479100338_n.jpg?stp=dst-jpg_p180x540&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=UVa9_r-2CJoAX-9YlQb&_nc_ht=scontent.fgye1-2.fna&oh=00_AT9mnY0TUq_NqVcsHcx2Vs38I0R2wzo0olRqMdlR0VPWFg&oe=6335BFA7",
-                      subtitle: "We have the right hat for everyone.",
-                      default_action: {
-                        type: "web_url",
-                        url: "https://www.facebook.com/photo/?fbid=620241313083539&set=a.356785789429094",
-                        webview_height_ratio: "tall",
-                      },
-                      buttons: [
-                        {
-                          type: "web_url",
-                          url: "https://www.facebook.com/photo/?fbid=620241313083539&set=a.356785789429094",
-                          title: "View Website",
-                        },
-                        {
-                          type: "postback",
-                          title: "Start Chatting",
-                          payload: "DEVELOPER_DEFINED_PAYLOAD",
-                        },
-                      ],
-                    },
-                    {
-                      title: "Welcome!",
-                      image_url:
-                        "https://i.blogs.es/30d986/cyberpunk-edgerunners/1366_2000.jpeg",
-                      subtitle: "We have the right hat for everyone.",
-                      default_action: {
-                        type: "web_url",
-                        url: "https://extrajaen.com/universidad/un-bot-ayudara-a-adolescentes-a-hablar-de-sus-emociones",
-                        webview_height_ratio: "tall",
-                      },
-                      buttons: [
-                        {
-                          type: "web_url",
-                          url: "https://extrajaen.com/universidad/un-bot-ayudara-a-adolescentes-a-hablar-de-sus-emociones",
-                          title: "View Website",
-                        },
-                        {
-                          type: "postback",
-                          title: "Start Chatting",
-                          payload: "DEVELOPER_DEFINED_PAYLOAD",
-                        },
-                      ],
-                    },
-                  ],
-                },
-              }, */
-              /* text: `Solo se repetir el mensaje: a`, */
-              /* attachment: {
-                type: "image",
-                payload: {
-                  url: "https://assets.puzzlefactory.pl/puzzle/316/243/original.jpg",
-                  is_reusable: true,
-                },
-              }, */
-              /* quick_replies: [
-                {
-                  content_type: "text",
-                  title: "Red",
-                  payload: "<POSTBACK_PAYLOAD>",
-                  image_url:
-                    "https://assets.puzzlefactory.pl/puzzle/316/243/original.jpg",
-                },
-                {
-                  content_type: "text",
-                  title: "Green",
-                  payload: "<POSTBACK_PAYLOAD>",
-                  image_url:
-                    "https://i.pinimg.com/564x/f6/e1/90/f6e190ba3a2c8646e634908ba2e1443d.jpg",
-                },
-              ], */
+              text: `Solo se repetir el mensaje: ${messageText}`,
             },
           };
 
           request(
             {
               uri: "https://graph.facebook.com/v2.6/me/messages",
-              qs: {
-                access_token: process.env.APP_TOKEN,
-              },
+              qs: { access_token: process.env.APP_TOKEN },
               method: "POST",
-              json: messageResponse,
+              json: messageResponseData,
             },
             function (error, response, data) {
               if (error) {
                 console.log("No fue posible enviar el mensaje");
               } else {
                 console.log("Mensaje enviado");
-
-                console.log(error);
               }
             }
           );
