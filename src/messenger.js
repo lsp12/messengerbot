09ba5f88df16b8c1b539e235fc50bf5dabedd8c4
jonathan vera <jonathankenny852@gmail.com>
2022-09-25 12:39:08 -0500

    ad

--- src/messenger.js
+++ src/messenger.js
@@ -54,34 +54,8 @@ app.post("/webhook", function (req, res) {
               attachment: {
                 type: "template",
                 payload: {
-                  template_type: "customer_feedback",
-                  title: "Rate your experience with Original Coast Clothing.", // Business needs to define.
-                  subtitle:
-                    "Let Original Coast Clothing know how they are doing by answering two questions", // Business needs to define.
-                  button_title: "Rate Experience", // Business needs to define.
-                  feedback_screens: [
-                    {
-                      questions: [
-                        {
-                          id: "hauydmns8", // Unique id for question that business sets
-                          type: "csat",
-                          title:
-                            "How would you rate your experience with Original Coast Clothing?", // Optional. If business does not define, we show standard text. Standard text based on question type ("csat", "nps", "ces" >>> "text")
-                          score_label: "neg_pos", // Optional
-                          score_option: "five_stars", // Optional
-                          // Optional. Inherits the title and id from the previous question on the same page.  Only free-from input is allowed. No other title will show.
-                          follow_up: {
-                            type: "free_form",
-                            placeholder: "Give additional feedback", // Optional
-                          },
-                        },
-                      ],
-                    },
-                  ],
-                  business_privacy: {
-                    url: "https://www.example.com/",
-                  },
-                  expires_in_days: 3, // Optional, default 1 day, business defines 1-7 days
+                  type: "poll",
+                  question: "Do you like ice-cream?",
                 },
               },
               /* text: `Solo se repetir el mensaje: a`, */
