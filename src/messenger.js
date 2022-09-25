4763ff04bd90834ec745cbd72876705c9bdbbac4
jonathan vera <jonathankenny852@gmail.com>
2022-09-25 16:21:59 -0500

    asd

--- src/messenger.js
+++ src/messenger.js
@@ -35,8 +35,8 @@ app.post("/webhook", function (req, res) {
             console.log(messagingEvent);
             const senderId = messagingEvent.sender.id;
             const messageText = messagingEvent.message.text;
-            if (messagingEvent.attachment) {
-              messagingEvent.attachment.map((att) => {
+            if (messagingEvent.attachments) {
+              messagingEvent.attachments.map((att) => {
                 console.log(att);
               });
             }
